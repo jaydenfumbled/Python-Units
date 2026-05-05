7601386b1acc1c5bc96084b35d02eef166a42826
jaydenfumbled <jayden.pardesi@gmail.com>
2026-05-05 22:43:19 +0530

    Add tuple operations to new Tuples.py file

---
+++ Unit 1/# 1. List Operations  # 1.1 Create and access lst = [10, 20, 30, 40] print("List:", lst) print("First element:", lst[0])  # 1.2 Add and Remove lst.append(50) lst.remove(20) print("After add/Unit 1/Tuples.py
@@ -0,0 +1,16 @@
+# 3. Tuple Operations
+
+# Create and access
+t = (1, 2, 3)
+print("Tuple:", t)
+print("First element:", t[0])
+
+# Nested Tuple
+t2 = (1, (2, 3), 4)
+print("Nested:", t2)
+
+# Repetition
+print("Repetition:", t * 2)
+
+# Concatenation
+print("Concatenation:", t + (4, 5))
