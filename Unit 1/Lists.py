e093b396abb798c122d99a4bab4d9c8218b30d3e
jaydenfumbled <jayden.pardesi@gmail.com>
2026-05-05 22:41:26 +0530

    Create Lists.py

---
+++ Unit 1/Lists.py
@@ -0,0 +1,19 @@
+# 1. List Operations
+
+# 1.1 Create and access
+lst = [10, 20, 30, 40]
+print("List:", lst)
+print("First element:", lst[0])
+
+# 1.2 Add and Remove
+lst.append(50)
+lst.remove(20)
+print("After add/remove:", lst)
+
+# 1.3 Sort
+lst.sort()
+print("Sorted:", lst)
+
+# 1.4 Reverse
+lst.reverse()
+print("Reversed:", lst)
